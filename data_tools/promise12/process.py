6faab1392fbfd8723981e17c210722c9123892a4
fanqiliang <1035847702@qq.com>
2022-05-15 11:51:47 +0800

    update

--- data_tools/promise12/process.py
+++ data_tools/promise12/process.py
@@ -14,39 +14,21 @@ import matplotlib.pyplot as plt
 # seg_output = r"D:\dataset\promise2012\seg_images"
 
 train_root = "/home/fanqiliang/data/TrainData"
+# output_root = "/home/fanqiliang/data/processed_data/v2" # 偏域修正 + 归一化
+# output_root = "/home/fanqiliang/data/processed_data/v3" # 单张图片标准化
+# output_root = "/home/fanqiliang/data/processed_data/v4" # 偏域修正 + 单张图片标准化 + 归一化
+# output_root = "/home/fanqiliang/data/processed_data/hist"  # 直方图均衡
+output_root = "/home/fanqiliang/data/processed_data/hist_test_50_8"  # 直方图均衡
+
+
+SIZE = 128
+Z_SIZE = 64
+
 # output_root = "/home/fanqiliang/data/processed_data/2normalized_data"
-output_root = "/home/fanqiliang/data/processed_data/3denoised_data"
+# output_root = "/home/fanqiliang/data/processed_data/1self_std"
 
-dst_size = np.asarray([128, 128, 64], dtype=np.float32)
-dst_sp = np.asarray([1, 1, 1.5], dtype=np.float64)
-
-def vnet_preprocess(img):
-    space = np.asarray(img.GetSpacing())
-    shape = np.asarray(img.GetSize())
-
-    factor = space / dst_sp
-    factorSize = np.asarray(shape * factor, dtype=float)
-    newSize = np.max([factorSize, dst_size], axis=0).astype(dtype=np.int32)
-
-    resampler = sitk.ResampleImageFilter()
-    resampler.SetReferenceImage(img)
-    resampler.SetOutputSpacing([dst_sp[0], dst_sp[1], dst_sp[2]])
-    resampler.SetSize(newSize.tolist())
-    resampler.SetInterpolator(sitk.sitkLinear)
-    imgResampled = resampler.Execute(img)
-
-    imgCentroid = np.asarray(newSize, dtype=float) / 2.0
-
-    imgStartPx = (imgCentroid - dst_size / 2.0).astype(dtype='int')
-
-    regionExtractor = sitk.RegionOfInterestImageFilter()
-    size_2_set = dst_size.astype(dtype='int')
-    regionExtractor.SetSize(size_2_set.tolist())
-    regionExtractor.SetIndex(imgStartPx.tolist())
-
-    imgResampledCropped = regionExtractor.Execute(imgResampled)
-
-    return imgResampledCropped
+if not os.path.exists(output_root):
+    os.makedirs(output_root, exist_ok=True)
 
 def process(idx, file, seg):
     img = sitk.ReadImage(file)
@@ -60,35 +42,46 @@ def process(idx, file, seg):
     img = sitk.Cast(img, sitk.sitkFloat32)
     seg_img = sitk.Cast(seg_img, sitk.sitkUInt8)
 
-    # mask_img = sitk.OtsuThreshold(img, 0, 1, 200)
+    mask_img = sitk.OtsuThreshold(img, 0, 1, 200)
     corrector = sitk.N4BiasFieldCorrectionImageFilter()
-    corrector.SetMaximumNumberOfIterations([200] * 4)
+    corrector.SetMaximumNumberOfIterations([50] * 8)
     img = corrector.Execute(img, seg_img)
+
+    # 直方图均衡
+    # filter = sitk.AdaptiveHistogramEqualizationImageFilter()
+    # img = filter.Execute(img)
 
     assert img.GetSize() == seg_img.GetSize()
     assert img.GetSpacing() == seg_img.GetSpacing()
     
-    img = vnet_preprocess(img)
-    seg_img = vnet_preprocess(seg_img)
     arr = sitk.GetArrayFromImage(img)
     seg_arr = sitk.GetArrayFromImage(seg_img)
+    # assert arr.shape[2] == dst_size[0] and arr.shape[1] == dst_size[1] and arr.shape[0] == dst_size[2]
+    seg_space = seg_img.GetSpacing()
+    seg_shape = seg_img.GetSize()
 
+    z_shape = arr.shape[0]
+    for i in range(1, 10):
+        if abs(z_shape - 2 ** i) < abs(z_shape - 2 ** (i+1)):
+            z_size = 2 ** i
+            break
     #
     # resample
     #
-    # resampler = sitk.ResampleImageFilter()
-    # resampler.SetReferenceImage(img)
-    # resampler.SetOutputSpacing([seg_space[0] * seg_shape[0] / 128, seg_space[1] * seg_shape[1] / 128, seg_space[2] * seg_shape[2] / 65])
-    # resampler.SetSize((128, 128, 65))
-    # resampler.SetInterpolator(sitk.sitkNearestNeighbor)
-    # seg_img = resampler.Execute(seg_img)
-    # seg_arr = sitk.GetArrayFromImage(seg_img)[:-1]
-    # img = resampler.Execute(img)
-    # arr = sitk.GetArrayFromImage(img)[:-1]
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(img)
+    resampler.SetOutputSpacing([seg_space[0] * seg_shape[0] / SIZE, seg_space[1] * seg_shape[1] / SIZE, seg_space[2] * seg_shape[2] / (z_size+1)])
+    resampler.SetSize((SIZE, SIZE, (z_size+1)))
+    
+    resampler.SetInterpolator(sitk.sitkNearestNeighbor)
+    seg_img = resampler.Execute(seg_img)
+    seg_arr = sitk.GetArrayFromImage(seg_img)[:-1]
+    img = resampler.Execute(img)
+    arr = sitk.GetArrayFromImage(img)[:-1]
 
-    # arr = zoom(arr, (64 / arr.shape[0], 128 / arr.shape[1], 128 / arr.shape[2])).astype(np.float32)
-    # seg_arr = zoom(seg_arr, (64 / seg_arr.shape[0], 128 / seg_arr.shape[1], 128 / seg_arr.shape[2]), mode='nearest')
-    # seg_arr = np.where(seg_arr > 0.5, 1, 0).astype(int)
+
+    # arr = zoom(arr, (64 / arr.shape[0], SIZE / arr.shape[1], SIZE / arr.shape[2])).astype(np.float32)
+    # seg_arr = zoom(seg_arr, (64 / seg_arr.shape[0], SIZE / seg_arr.shape[1], SIZE / seg_arr.shape[2]), mode='nearest')
 
     # for i, slice in enumerate(arr):
     #     fig = plt.figure()
@@ -98,20 +91,33 @@ def process(idx, file, seg):
     #     plt.savefig(os.path.join(img_output, f"{idx}_{i}.png"), bbox_inches="tight")
     #     plt.close(fig)
 
-    # arr = (arr - arr[arr > 0].mean()) / arr[arr > 0].std()
-    arr = (arr - mean) / std
+
+    mean = arr.mean()
+    std = arr.std()
+
+    # arr = (arr - mean) / std
     arr = (arr - arr.min()) / (arr.max() - arr.min())
+    # arr = (arr - arr[arr > 0].mean()) / arr[arr > 0].std()
+    seg_arr = np.where(seg_arr > 0.5, 1, 0).astype(int)
 
     sitk.WriteImage(sitk.GetImageFromArray(arr), os.path.join(output_root, f"{idx}.mhd"))
     sitk.WriteImage(sitk.GetImageFromArray(seg_arr), os.path.join(output_root, f"{idx}_seg.mhd"))
+    # return arr.flatten()
 
 if __name__ == "__main__":
     seg_file = glob(os.path.join(train_root, "*_segmentation*.mhd"))
     file = [f.replace("_segmentation", "") for f in seg_file]
     arrs = np.concatenate([sitk.GetArrayFromImage(sitk.ReadImage(f)).flatten() for f in file], axis=0)
     
-    mean = arrs[arrs > 0].mean()
-    std = arrs[arrs > 0].std()
+    # mean = arrs[arrs > 0].mean()
+    # std = arrs[arrs > 0].std()
+    # mean = 281.41382
+    # std = 340.0665
 
-    with Pool(1) as pool:
+    with Pool(8) as pool:
         pool.starmap(process, [(idx, file[idx], seg_file[idx]) for idx in range(len(seg_file))])
+        pool.close()
+        pool.join()
+        # result = pool.starmap(process, [(idx, file[idx], seg_file[idx]) for idx in range(len(seg_file))])
+    # r = np.concatenate(result)
+    # print(r.mean(), r.std())
---
+++ data_tools/promise12/process_test.py
@@ -0,0 +1,52 @@
+import SimpleITK as sitk
+import os
+from glob import glob
+from scipy.ndimage import zoom, binary_fill_holes
+import numpy as np
+from multiprocessing import Pool
+import matplotlib.pyplot as plt
+
+test_root = "/home/fanqiliang/data/TestData"
+output_root = "/home/fanqiliang/data/processed_data/processed_test"
+
+if not os.path.exists(output_root):
+    os.makedirs(output_root, exist_ok=True)
+
+def do(file: str):
+    output_file = os.path.join(output_root, os.path.basename(file))
+
+    img = sitk.ReadImage(file)
+    img = sitk.Cast(img, sitk.sitkFloat32)
+    mask_img = sitk.OtsuThreshold(img, 0, 1, 200)
+    corrector = sitk.N4BiasFieldCorrectionImageFilter()
+    corrector.SetMaximumNumberOfIterations([50] * 8)
+    img = corrector.Execute(img, mask_img)
+    
+    arr = sitk.GetArrayFromImage(img)
+
+    z_shape = arr.shape[0]
+    for i in range(1, 10):
+        if abs(z_shape - 2 ** i) < abs(z_shape - 2 ** (i+1)):
+            z_size = 2 ** i
+            break
+
+    resampler = sitk.ResampleImageFilter()
+    resampler.SetReferenceImage(img)
+    resampler.SetSize((128, 128, (z_size+1)))
+
+    img = resampler.Execute(img)
+    arr = sitk.GetArrayFromImage(img)[:-1]
+
+    arr = (arr - arr.min()) / (arr.max() - arr.min())
+    
+    sitk.WriteImage(sitk.GetImageFromArray(arr), output_file)
+    print(file)
+    return file
+
+if __name__ == "__main__":
+    test_file = glob(os.path.join(test_root, "*.mhd"))
+
+    with Pool(processes=8) as pool:
+        pool.map(do, test_file, chunksize=8)
+        pool.close()
+        pool.join()
